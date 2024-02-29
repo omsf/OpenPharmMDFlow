f9d273fab2cf68dff81813ed1ca99bcd79ca3449
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2024-02-29 12:46:05 -0700

    added helper function to read in cif

--- openpharmmdflow/io/cif.py
+++ openpharmmdflow/io/cif.py
@@ -2,10 +2,52 @@
 Module for cif file operations
 """
 import os
+import shutil
+import subprocess
+import tempfile
 
 import openff.toolkit
 
 
-def from_cif(path: str | os.PathLike) -> openff.toolkit.Topology:
-    topology = openff.toolkit.Topology()
-    return topology
+def from_cif(path: str | os.PathLike) -> openff.toolkit.Molecule:
+    """
+    Convert a Crystallographic Information File (CIF) to an OpenFF Toolkit Molecule.
+
+    Parameters
+    ----------
+    path : str or os.PathLike
+        The path to the CIF file.
+
+    Returns
+    -------
+    openff.toolkit.Molecule
+        An OpenFF Toolkit Molecule object representing the molecular structure.
+
+    Raises
+    ------
+    RuntimeError
+        If the required external tool 'codcif2sdf' is not found.
+
+    Notes
+    -----
+    This function utilizes the 'codcif2sdf' external tool to convert a CIF file
+    to a Structure Data File (SDF), and then reads the SDF file into an OpenFF
+    Toolkit Molecule object. Bond order is guessed.
+
+    Examples
+    --------
+    >>> from openpharmmdflow import from_cif
+    >>> molecule = from_cif("/path/to/example.cif")
+    >>> molecule.visualize()
+    """
+
+    if shutil.which("codcif2sdf") is None:
+        raise RuntimeError("codcif2sdf is required to read cif")
+    with tempfile.TemporaryDirectory() as td:
+        out_path = f"{td}/out.sdf"
+        cmd_output = subprocess.run(
+            ["codcif2sdf", f"{path}", ">", out_path], capture_output=True, check=True
+        )
+        molecule = openff.toolkit.Molecule.from_file(out_path)
+
+    return molecule
