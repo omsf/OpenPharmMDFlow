785ad41d19d4bf716b6a2ec40c710a8f1214285d
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2024-04-25 03:53:14 -0700

    updated tests

--- openpharmmdflow/tests/io/cif_test.py
+++ openpharmmdflow/tests/io/cif_test.py
@@ -1,28 +1,34 @@
 from importlib import resources
 
 import pytest
+from openff.toolkit.utils.exceptions import MoleculeParseError
 
 from openpharmmdflow import from_cif
 
+IO_DATA_PATH = resources.files("openpharmmdflow.tests.io.data")
 
-@pytest.fixture()
+
 def AWUDEB_cif():
-    with resources.files("openpharmmdflow.tests.io.data") as d:
-        yield str(d / "AWUDEB.cif")
+    return IO_DATA_PATH / "AWUDEB.cif"
 
 
-@pytest.fixture()
 def HXACAN_cif():
-    with resources.files("openpharmmdflow.tests.io.data") as d:
-        yield str(d / "HXACAN.cif")
+    return IO_DATA_PATH / "HXACAN.cif"
+
+
+def TRICKY_cif():
+    # From https://www.crystallography.net/cod/2102215.html
+    return IO_DATA_PATH / "2102215.cif"
 
 
 @pytest.mark.parametrize(
-    "cif_file, should_load", [(HXACAN_cif, True), (AWUDEB_cif, True)]
+    "cif_file, should_load",
+    [(HXACAN_cif(), True), (AWUDEB_cif(), True), (TRICKY_cif(), False)],
 )
 def test_cif_loading_defaults(cif_file, should_load):
+    print(cif_file)
     if should_load:
         from_cif(cif_file)
     else:
-        with pytest.raises(RuntimeError):
+        with pytest.raises(MoleculeParseError):
             from_cif(cif_file)
