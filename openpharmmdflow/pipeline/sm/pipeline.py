c5cdb3c2b14633b044a0bf4880ce83c7b4379939
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2025-02-07 16:06:22 -0700

    add solvation and also a large mol pipeline example (#70)
    * add solvation and also a large mol pipeline example
    
    * Adding first stab at solvating and running a protein simulation
    
    * added TODOs for later PRs
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * updated notebooks to match api
    
    ---------
    
    Co-authored-by: Stephen Thomas <stephen.thomas@bms.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- openpharmmdflow/pipeline/sm/pipeline.py
+++ openpharmmdflow/pipeline/sm/pipeline.py
@@ -2,9 +2,13 @@
 Small Molecule Pipeline
 """
 
+import os
+
 from openff.interchange import Interchange
 from openff.interchange.components._packmol import pack_box
+from openff.interchange.components._packmol import solvate_topology
 from openff.toolkit import ForceField
+from openff.toolkit import Molecule
 
 from openpharmmdflow.bespokefit import build_bespoke_workflow_factory
 from openpharmmdflow.bespokefit import run_bespokefit
@@ -29,6 +33,7 @@ class SmallMoleculePipeline:
         )
         self.prep_config = config.prep_config if config.prep_config else None
         self.pack_config = config.pack_config
+        self.solvate_config = config.solvate_config if config.solvate_config else None
         self.parameterize_config = config.parameterize_config
         self.bespoke_ff = None
         self.simulate_config = config.simulate_config
@@ -69,6 +74,42 @@ class SmallMoleculePipeline:
             box_shape=self.pack_config.box_shape,
         )
 
+    def solvate(self):
+        # Solvate the box
+        self.solvated_topology = solvate_topology(
+            self.topology,
+            nacl_conc=self.solvate_config.nacl_conc,
+            padding=self.solvate_config.padding,
+            box_shape=self.solvate_config.box_shape,
+            target_density=self.solvate_config.target_density,
+            tolerance=self.solvate_config.tolerance,
+        )
+        # create a water molecule
+        self.water = Molecule.from_smiles("O")
+        self.water.generate_conformers(n_conformers=1)
+        # TODO See if we can get the number of waters, Na, Cl out of solvate_topology
+        # TODO Maybe import water + ions instead of making them here?
+        # find out the number of waters in the system
+        self.n_water = len(
+            [
+                m
+                for m in self.solvated_topology.molecules
+                if m.to_smiles() == "[H][O][H]"
+            ]
+        )
+        # create a Sodium ion
+        self.sodium_ion = Molecule.from_smiles("[Na+]")
+        # find out the number of sodium ions in the system
+        self.n_sodium_ion = len(
+            [m for m in self.solvated_topology.molecules if m.to_smiles() == "[Na+]"]
+        )
+        # create a Chlorine ion
+        self.chlorine_ion = Molecule.from_smiles("[Cl-]")
+        # find out the number of sodium ions in the system
+        self.n_chlorine_ion = len(
+            [m for m in self.solvated_topology.molecules if m.to_smiles() == "[Cl-]"]
+        )
+
     def parameterize(self):
         # TODO test to make sure we use the FF we expect to use
         # Use bespoke ff if we made one
@@ -78,9 +119,28 @@ class SmallMoleculePipeline:
         # Now if force_field is a path or string, we need to turn it into a ForceField object
         if not isinstance(self.force_field, ForceField):
             self.force_field = ForceField(self.force_field)
-        self.interchange = Interchange.from_smirnoff(
+        self.components_intrcg = Interchange.from_smirnoff(
             force_field=self.force_field, topology=self.topology
         )
+        # if there are waters built during the solvate step combine the components topology
+        # with the water topology
+        # TODO better way to check if we ran solvate?
+        # TODO let user specify ff for solute
+        if hasattr(self, "water"):
+            self.water_intrcg = Interchange.from_smirnoff(
+                force_field=ForceField("openff_unconstrained-2.0.0.offxml"),
+                topology=[self.water] * self.n_water
+                + [self.sodium_ion] * self.n_sodium_ion
+                + [self.chlorine_ion] * self.n_chlorine_ion,
+            )
+            # combine is still experimental
+            os.environ["INTERCHANGE_EXPERIMENTAL"] = "1"
+            self.interchange = self.components_intrcg.combine(self.water_intrcg)
+            self.interchange.positions = self.solvated_topology.get_positions()
+            self.interchange.box = self.solvated_topology.box_vectors
+
+        else:
+            self.interchange = self.components_intrcg
 
     def simulate(self):
         self.simulation = create_simulation(self.simulate_config, self.interchange)
