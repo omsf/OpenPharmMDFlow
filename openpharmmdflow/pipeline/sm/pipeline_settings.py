f9c8b9897a0c969be88e1273e3a3d237adefcae4
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2025-06-09 14:21:07 -0700

    remove confusing imports

--- openpharmmdflow/pipeline/sm/pipeline_settings.py
+++ openpharmmdflow/pipeline/sm/pipeline_settings.py
@@ -11,9 +11,7 @@ from openff.interchange.components._packmol import UNIT_CUBE
 from openff.models.types import FloatQuantity
 from openff.models.types import Quantity
 from openff.toolkit import ForceField
-from openff.units import Quantity
 from pydantic import BaseModel
-from pydantic import model_validator
 
 
 class SmallMoleculePipelineInputConfig(BaseModel):
